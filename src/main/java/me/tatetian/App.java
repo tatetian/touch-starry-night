33dadcab4acffc1aaeadfda9882516ac2a14d111
Hongliang TIAN <tatetian@gmail.com>
2013-05-21 17:01:45 +0800

    Simple Processing program

--- src/main/java/me/tatetian/App.java
+++ src/main/java/me/tatetian/App.java
@@ -1,13 +1,18 @@
 package me.tatetian;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import processing.core.*;
+
+public class App extends PApplet
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public void setup() {
+      size(200,200);
+      background(0);
+    }
+
+    public void draw() {
+      stroke(255);
+      if (mousePressed) {
+        line(mouseX,mouseY,pmouseX,pmouseY);
+      }
     }
 }
