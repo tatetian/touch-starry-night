abb77cd6bf9faf00ac7320c333a6e3e06ac8d9cc
Hongliang TIAN <tatetian@gmail.com>
2013-05-23 15:32:29 +0800

    Fix slow rendering issue of main scene

--- src/main/java/me/tatetian/App.java
+++ src/main/java/me/tatetian/App.java
@@ -8,11 +8,11 @@ public class App extends PApplet
 	private static final String BASE_DIR = "../../";
 
 	// Parameters related animation
-	private static final float MAX_ZOOM_GRASS = 1.3f, 
-														 MAX_ZOOM_TREES = 1.2f, 
-														 MAX_ZOOM_HILLS = 1.1f, 
-														 MAX_ZOOM_STARS = 1.05f;
-	private static final int ANIMATION_PERIOD_MS = 30 * 1000; 
+	private static final float MAX_ZOOM_GRASS = 1.5f, 
+														 MAX_ZOOM_TREES = 1.3f, 
+														 MAX_ZOOM_HILLS = 1.2f, 
+														 MAX_ZOOM_STARS = 1.1f;
+	private static final int ANIMATION_PERIOD_MS = 20 * 1000; 
 	
 	private static final int SCREEN_WIDTH  = 1440;
 	private static final int SCREEN_HEIGHT = 810; 
@@ -38,37 +38,44 @@ public class App extends PApplet
   	int now = millis();
   	int t = now % ANIMATION_PERIOD_MS;
   	animate(t);
-  	System.out.println("frame rate = " + frameRate);
+  	//System.out.println("frame rate = " + frameRate);
   }
   
   // t \in [0, ANIMATION_PERIOD_MS)
   private void animate(int t) {
+  	translate(CENTER_X, CENTER_Y);
+  	
   	animateObject(t, imgStars, MAX_ZOOM_STARS);  	
   	animateObject(t, imgHills, MAX_ZOOM_HILLS);
   	animateObject(t, imgTrees, MAX_ZOOM_TREES);
   	animateObject(t, imgGrass, MAX_ZOOM_GRASS);
   }
   
-  private void animateObject(int t, PImage img, float maxZoom) {  	
-  	float animZoom = f(t, maxZoom);
+  private void animateObject(int t, PImage img, float maxZoom) {
   	
-  	float zoomDelta = animZoom - 1.0f;
-  	int x = (int) (-CENTER_X * zoomDelta);
-  	int y = (int) (-CENTER_Y * zoomDelta);
-  	image(img, x, y, (int) (animZoom * SCREEN_WIDTH), (int) (animZoom * SCREEN_HEIGHT));
+  	//float animZoom = f(t, maxZoom);
+  	
+//  	float zoomDelta = animZoom - 1.0f;
+//  	int x = (int) (-CENTER_X * zoomDelta);
+//  	int y = (int) (-CENTER_Y * zoomDelta);
+  	pushMatrix();
+  		float animZoom = f(t, maxZoom);
+  		scale(animZoom);
+  		image(img, -CENTER_X, -CENTER_Y);
+  	popMatrix();
   }
   
   private float f(int t, float maxZoom) {
   	assert(t >= 0 && t < ANIMATION_PERIOD_MS);
   	
-  	float speed = 2 * (maxZoom - 1.0f) / ANIMATION_PERIOD_MS;
+  	double speed = 2 * (maxZoom - 1.0) / ANIMATION_PERIOD_MS;
   	// camera move forward
   	if(t <= ANIMATION_PERIOD_MS / 2) {
-  		return 1.0f + speed * t;
+  		return (float) (1.0 + speed * t);
   	}
   	// camera move backward
   	else {
-  		return 1.0f + speed * (ANIMATION_PERIOD_MS - t);
+  		return (float) (1.0 + speed * (ANIMATION_PERIOD_MS - t) );
   	}
   } 
 }
