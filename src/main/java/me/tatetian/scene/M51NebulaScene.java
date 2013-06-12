46153e5db10f0f6380f8dc2d9d464f2822d643ed
Hongliang TIAN <tatetian@gmail.com>
2013-06-12 14:25:43 +0800

    M51 scene is runnable

--- src/main/java/me/tatetian/scene/M51NebulaScene.java
+++ src/main/java/me/tatetian/scene/M51NebulaScene.java
@@ -3,6 +3,7 @@ package me.tatetian.scene;
 import me.tatetian.Engine;
 import me.tatetian.stars.Nebula;
 import me.tatetian.stars.SpriteStarsRenderer;
+import me.tatetian.stars.Star;
 import me.tatetian.stars.StarsGenerator;
 import processing.core.PImage;
 import processing.opengl.PShader;
@@ -10,6 +11,9 @@ import processing.opengl.PShader;
 public class M51NebulaScene extends NebulaScene {
 	private Nebula[] nebulas;
 	private PImage background;
+	private float angle;
+	private float z_adjust;
+	private float z_step;
 	
 	@Override
 	protected void setup() {
@@ -17,24 +21,57 @@ public class M51NebulaScene extends NebulaScene {
 		// init background
 		background = E.loadImage(E.BASE_PATH + "max.png");
 		// init nubulas
-		nebulas = new Nebula[1];
+		nebulas = new Nebula[7];
 		nebulas[0] = new Nebula(
-				StarsGenerator.generate(300, E.width, E.height, 1000, 2000), 
+				StarsGenerator.generate(5, E.width, E.height, 1.5f, -1600, -800), 
 				new SpriteStarsRenderer(E.BASE_PATH + "small_stars/1.png", 100) );
+		nebulas[1] = new Nebula(
+				StarsGenerator.generate(10, E.width , E.height, 1.5f, -1600, -800), 
+				new SpriteStarsRenderer(E.BASE_PATH + "small_stars/2.png", 100) );
+		nebulas[2] = new Nebula(
+				StarsGenerator.generate(2, E.width, E.height, 1.5f, -800, -600), 
+				new SpriteStarsRenderer(E.BASE_PATH + "small_stars/3.png", 100) );
+		nebulas[3] = new Nebula(
+				StarsGenerator.generate(25, E.width, E.height, 1.75f, -1200, -800), 
+				new SpriteStarsRenderer(E.BASE_PATH + "small_stars/4.png", 100) );
+		nebulas[4] = new Nebula(
+				StarsGenerator.generate(10, E.width, E.height, 1.5f, -1600, -1200), 
+				new SpriteStarsRenderer(E.BASE_PATH + "small_stars/5.png", 100) );
+		nebulas[5] = new Nebula(
+				StarsGenerator.generate(6, E.width, E.height, 1.5f, -3000, -2000), 
+				new SpriteStarsRenderer(E.BASE_PATH + "small_stars/6.png", 100) );
+		nebulas[6] = new Nebula(
+				StarsGenerator.generate(10, E.width, E.height, 2f, -2000, -800), 
+				new SpriteStarsRenderer(E.BASE_PATH + "small_stars/7.png", 100) );
+		
+		angle 		= 0.0f;
+		z_adjust  = 0.0f;
+		z_step 		= 0.25f;
 	}
 
 	@Override
-	protected void _draw() {		
-		// draw background
-		//e.blendMode(e.REPLACE);		
-		Engine E = Engine.INSTANCE;
+	protected void _draw() {
+		Engine E = Engine.INSTANCE;	
+		// draw nebula background
 		E.pushMatrix();
-		E.translate(0, 0, - E.WINDOW_DEPTH / 10);
+		E.translate(0, 0, - E.WINDOW_DEPTH / 10);		
+		E.translate(E.WINDOW_WIDTH/2, E.WINDOW_HEIGHT/2, 0);		
+		E.rotateZ(angle * 0.75f);		
+		E.translate(-E.WINDOW_WIDTH/2, -E.WINDOW_HEIGHT/2, z_adjust);
 		E.image(background, E.WINDOW_WIDTH / 2, E.WINDOW_HEIGHT / 2);
 		E.popMatrix();
-		// draw nebulas
+		// draw stars
+		E.translate(E.WINDOW_WIDTH/2, E.WINDOW_HEIGHT/2, 0);		
+		E.rotateZ(angle);		
+		E.translate(-E.WINDOW_WIDTH/2, -E.WINDOW_HEIGHT/2, z_adjust);
 		for(Nebula nb : nebulas) {
 			nb.draw();
 		}
+		// animate
+		angle += 0.0004;
+		if(angle > 2 * E.PI) angle -= 2 * E.PI;
+		z_adjust += z_step;
+		if(z_adjust > 600) { z_adjust = 600; z_step *= -1; }
+		else if(z_adjust < 0) { z_adjust = 0; z_step *= -1; }
 	}
 }
--- src/main/java/me/tatetian/stars/StarsGenerator.java
+++ src/main/java/me/tatetian/stars/StarsGenerator.java
@@ -10,19 +10,30 @@ public class StarsGenerator {
 	} 
 	
 	public static Star[] generate(int num, 
-																float max_x, float max_y, 
+																float w, float h, float xy_scale, 
 																float min_z, float max_z) {
 		Star[] stars = new Star[num];
+		float min_x = w * ( 1 - xy_scale ) / 2,
+					min_y = h * ( 1 - xy_scale ) / 2,
+					max_x = xy_scale * w,
+					max_y = xy_scale * h;
 		for(int star_i = 0; star_i < num; star_i++) {
-			stars[star_i] = generate(max_x, max_y, min_z, max_z);
+			stars[star_i] = generate(min_x, max_x, min_y, max_y, min_z, max_z);
 		}
 		return stars;
 	}
 	
-	private static Star generate(float max_x, float max_y, 
-															 float min_z, float max_z) {		
-		float x = random.nextFloat() * max_x,
-					y = random.nextFloat() * max_y,
+	private static Star generate(float min_x, float max_x, 
+															 float min_y, float max_y, 
+															 float min_z, float max_z) {	
+		float rx, ry;
+		do {
+			rx = random.nextFloat();
+			ry = random.nextFloat();
+		} while( 0.38f < rx && rx < 0.62f && 0.38f < ry && ry < 0.62f);
+		
+		float x = min_x + rx * (max_x - min_x),
+					y = min_y + ry * (max_y - min_y),
 					z = min_z + random.nextFloat() * (max_z - min_z);
 		return new Star(x, y, z); 
 	} 
